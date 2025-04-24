8086ae706310faa29829ea4707eb84a568b5a814
MDNabilAhmed <mdnabilahmed706@gmail.com>
2025-04-24 21:30:06 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './Navbar/Navbar'
 import Body from './Body/Body'
+import Footer from './Footer/Footer'
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Body />
+      <Footer />
     </div>
   )
 }
---
+++ src/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div>
+      <div className="background-container">
+
+      </div>
+    </div>
+  )
+}
+
+export default Footer
